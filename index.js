713acbd73e833677c36cb1041525716df73f63aa
frankzeye <frank.eybsen@gmail.com>
2016-04-12 08:01:02 -0700

    updated rocky

--- index.js
+++ index.js
@@ -31,11 +31,7 @@ controller.hears(['album'], ['direct_mention'], function (bot, message) {
   bot.reply(message, randomAlbum)
 })
 
-controller.hears(['rocky'], ['direct_message','direct_mention','mention','ambient'], function (bot, message) {
-  bot.reply(message, 'Honestly slackbot, I think you\'re being a bit harsh on the guy.')
-})
-
-controller.hears(['rocky'], ['direct_message','direct_mention','mention','ambient'], function (bot, message) {
+controller.hears(['rocky sucks'], ['direct_message','direct_mention','mention','ambient'], function (bot, message) {
   bot.reply(message, 'Honestly slackbot, I think you\'re being a bit harsh on the guy.')
 })
 
